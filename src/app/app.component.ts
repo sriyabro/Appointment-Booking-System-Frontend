3ff1b03e996b882aac11885490df10424f9a2929
Sriyanjith Herath <sriyabro@gmail.com>
2024-05-30 00:14:37 +0530

    app init changes - template code removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Appointment_Booking_System';
+  title = 'Appointment Booking System';
 }
